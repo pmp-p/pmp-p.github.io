223d84b84310540cb4111c2688f93eba3a13b259
pmp-p <mail.peny@free.fr>
2020-06-05 12:49:56 +0200

    better uweak sample

--- wapy/samples/test_weak.py
+++ wapy/samples/test_weak.py
@@ -1,60 +1,29 @@
+import sys
+import os
+import gc
 
+import embed
 
-a = 1
-b = 2
-c = 3
-d = 4
+a = 1;b = 2;c = 3
 
 def allocate_some():
-  d = [a] * 1024
-  e = [3] * 1024
-  f = [4] * 1024
+  d = [a] * 1024;  e = [3] * 1024;  f = [4] * 1024
 
 
-if 1:
-    import sys
-    import os
-    import gc
+class uweak:
 
-    import embed
+    def __init__(self):
+        def __del__():
+            print("uweak.__del__")
+        self.__del__ = embed.on_del( __del__ )
 
+
+def func():
+    x = uweak()
 
     def foo():
-      print('i am a finalizer')
-
-
-    def func():
-        x = embed.on_del(foo)
-        # x vanishes here
-
-else:
-
-    class uweak:
-        def __init__(self, cb, key):
-            self.key = key
-            self.cb = cb
-
-        def __del__(self):
-            self.cb( self.key )
-     #============================
-
-    def __del__(the_key_refing_a_pointer):
-          #ffi magic dealing with key to dtor the pointer
-         print("cleaned up", the_key_refing_a_pointer )
-
-    class user_def:
-        def __init__(self ):
-            self.weak =uweak(__del__, id(self))
-
-
-    def func():
-        x = user_def()
-        # x vanishes here
-
-
-
-
-
+        print('i am a foo finalizer')
+    z = embed.on_del(foo)
 
 
 func()
